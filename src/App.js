d069f23eef253923aa0696f310e59e5ad17e4978
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-07 14:10:13 -0600

    App and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import socketIO from 'socket.io-client';
+import './App.css';
+import ChatPage from './components/ChatPage';
+import Home from './components/Home';
+
+const socket = socketIO.connect('https://chat-app-node-back.onrender.com');
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />} />
+          <Route path="/chat" element={<ChatPage socket={socket} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
