42fda96542bfeef75fa31f4a5db0d5ac74a2960a
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-07 14:09:14 -0600

    Add ChatBody component

---
+++ src/components/ChatBody.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/forbid-prop-types */
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ChatBody = ({ messages, typingStatus, lastMessageRef }) => {
+  const navigate = useNavigate();
+
+  const handleLeaveChat = () => {
+    localStorage.removeItem('userName');
+    navigate('/');
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <header className="chat__mainHeader">
+        <p>Hangout with Colleagues</p>
+        <button type="button" className="leaveChat__btn" onClick={handleLeaveChat}>
+          LEAVE CHAT
+        </button>
+      </header>
+
+      <div className="message__container">
+        {messages.map((message) => (message.name === localStorage.getItem('userName') ? (
+          <div className="message__chats" key={message.id}>
+            <p className="sender__name">You</p>
+            <div className="message__sender">
+              <p>{message.text}</p>
+            </div>
+          </div>
+        ) : (
+          <div className="message__chats" key={message.id}>
+            <p>{message.name}</p>
+            <div className="message__recipient">
+              <p>{message.text}</p>
+            </div>
+          </div>
+        )))}
+
+        <div className="message__status">
+          <p>{typingStatus}</p>
+        </div>
+
+        {/* --- At the bottom of the JSX element ----*/}
+        <div ref={lastMessageRef} />
+      </div>
+    </>
+  );
+};
+
+ChatBody.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.object).isRequired,
+  typingStatus: PropTypes.string.isRequired,
+  lastMessageRef: PropTypes.object.isRequired,
+};
+
+export default ChatBody;
